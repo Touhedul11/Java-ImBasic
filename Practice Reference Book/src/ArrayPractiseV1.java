143ff4a49b2835b4566e5c7a16aa654082f4d5f7
Touhedul Islam <49379202+Touhedul11@users.noreply.github.com>
2020-07-27 12:28:21 -0400

    From reference book

---
+++ Practice Reference Book/src/ArrayPractiseV1.java
@@ -0,0 +1,10 @@
+
+public class ArrayPractiseV1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    byte month_days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    System.out.println("The month April has " + month_days[3] + " days");
+	}
+
+}
---
+++ Practice Reference Book/src/AryyyPractise.java
@@ -0,0 +1,26 @@
+
+public class AryyyPractise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   byte month_days [];
+   month_days = new byte[12];
+   
+           month_days[0]= 31;
+		   month_days[1]= 28;
+		   month_days[2]= 31;
+		   month_days[3]= 30;
+		   month_days[4]= 31;
+		   month_days[5]= 30;
+		   month_days[6]= 31;
+		   month_days[7]= 31;
+		   month_days[8]= 30;
+		   month_days[9]= 31;
+		   month_days[10]= 30;
+		   month_days[11]= 31;
+		   
+		   System.out.println("April has"+ month_days[3] + "days");
+		   // Now we can write the same program in different ways but will produce the same result
+	}
+
+}
